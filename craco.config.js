001c8080e66257d5c680151b5d16dba8e59e1885
宋德康 <1494403731@qq.com>
2021-08-31 15:42:21 +0800

    添加 compression-webpack-plugin 插件

--- craco.config.js
+++ craco.config.js
@@ -1,76 +1,83 @@
-const path = require('path');
-const CracoLessPlugin = require('craco-less');
-const CracoAntDesignPlugin = require('craco-antd');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const path = require('path')
+const CracoLessPlugin = require('craco-less')
+const CracoAntDesignPlugin = require('craco-antd')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin')
-const CircularDependencyPlugin = require('circular-dependency-plugin');
-const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin');
+const CircularDependencyPlugin = require('circular-dependency-plugin')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin')
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const pathResolve = pathUrl => path.join(__dirname, pathUrl);
+const pathResolve = (pathUrl) => path.join(__dirname, pathUrl)
 
-const { whenProd } = require('@craco/craco');
+const { whenProd } = require('@craco/craco')
 
 module.exports = {
-	webpack: {
-		// 别名配置
-		alias: {
-			'@': pathResolve('src'),
-		},
-		plugins: [
-			new CircularDependencyPlugin({
-				exclude: /node_modules/,
-				include: /src/,
-				failOnError: true,
-				allowAsyncCycles: false,
-				cwd: process.cwd(),
-			}),
-			// 查看打包的进度
-			new SimpleProgressWebpackPlugin(),
-			// 替换moment为dayjs
-			new AntdDayjsWebpackPlugin(),
-			// 依赖包分析
-			// new BundleAnalyzerPlugin(),
-			...whenProd(
-				() => [
-					new UglifyJsPlugin({
-						uglifyOptions: {
-							// 删除注释
-							output: {
-								comments: false,
-							},
-							compress: {
-								drop_console: true, // 删除所有调式带有console的
-								drop_debugger: true,
-								pure_funcs: ['console.log'], // 删除console.log
-							},
-						},
-					}),
-				],
-				[]
-			),
-		],
-		configure: (webpackConfig, { env, paths }) => {
-			paths.appBuild = 'dist';
-			webpackConfig.output = {
-				...webpackConfig.output,
-				path: path.resolve(__dirname, 'dist'),
-				// publicPath: "/",
-			};
-			return webpackConfig;
-		},
-	},
-	plugins: [
-		{ plugin: CracoAntDesignPlugin },
-		{
-			plugin: CracoLessPlugin,
-			options: {
-				lessLoaderOptions: {
-					lessOptions: {
-						// modifyVars: { '@primary-color': '#1DA57A' },
-						javascriptEnabled: true,
-					},
-				},
-			},
-		},
-	],
-};
+  webpack: {
+    // 别名配置
+    alias: {
+      '@': pathResolve('src'),
+    },
+    plugins: [
+      new CircularDependencyPlugin({
+        exclude: /node_modules/,
+        include: /src/,
+        failOnError: true,
+        allowAsyncCycles: false,
+        cwd: process.cwd(),
+      }),
+      // 查看打包的进度
+      new SimpleProgressWebpackPlugin(),
+      // 替换moment为dayjs
+      new AntdDayjsWebpackPlugin(),
+      // 依赖包分析
+      // new BundleAnalyzerPlugin(),
+      ...whenProd(
+        () => [
+          new UglifyJsPlugin({
+            uglifyOptions: {
+              // 删除注释
+              output: {
+                comments: false,
+              },
+              compress: {
+                drop_console: true, // 删除所有调式带有console的
+                drop_debugger: true,
+                pure_funcs: ['console.log'], // 删除console.log
+              },
+            },
+          }),
+          new CompressionWebpackPlugin({
+            algorithm: 'gzip',
+            test: new RegExp('.(' + ['js', 'css'].join('|') + ')$'),
+            threshold: 1024,
+            minRatio: 0.8,
+          }),
+        ],
+        []
+      ),
+    ],
+    configure: (webpackConfig, { env, paths }) => {
+      paths.appBuild = 'dist'
+      webpackConfig.output = {
+        ...webpackConfig.output,
+        path: path.resolve(__dirname, 'dist'),
+        // publicPath: "/",
+      }
+      return webpackConfig
+    },
+  },
+  plugins: [
+    { plugin: CracoAntDesignPlugin },
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            // modifyVars: { '@primary-color': '#1DA57A' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+}
