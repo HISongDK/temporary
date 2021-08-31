97e54f293418d90b56577a6969bbc22912c8c0c4
宋德康 <1494403731@qq.com>
2021-08-31 18:05:48 +0800

    no semi

--- src/pages/report/Report.jsx
+++ src/pages/report/Report.jsx
@@ -1,17 +1,17 @@
-import React, { useState, useRef, useCallback, useEffect, useReducer, useContext } from 'react';
-import { Modal, Table, Input, Form, Select, message, Dropdown, Menu, DatePicker } from 'antd';
+import React, { useState, useRef, useCallback, useEffect, useReducer, useContext } from 'react'
+import { Modal, Table, Input, Form, Select, message, Dropdown, Menu, DatePicker } from 'antd'
 // import moment from 'moment';
-import dayjs from 'dayjs';
-import { SearchOutlined, CaretDownOutlined } from '@ant-design/icons';
-import './report.less';
-import { debounce } from '@/util/debounce.js';
-import { UserContext } from '../../util/context';
-import { mediaQuery, newsQuery, newsAdd, newsUpdate } from '../../api';
+import dayjs from 'dayjs'
+import { SearchOutlined, CaretDownOutlined } from '@ant-design/icons'
+import './report.less'
+import { debounce } from '@/util/debounce.js'
+import { UserContext } from '../../util/context'
+import { mediaQuery, newsQuery, newsAdd, newsUpdate } from '../../api'
 
-const { Option } = Select;
+const { Option } = Select
 function Report() {
   /* 用户信息 */
-  const user = useContext(UserContext);
+  const user = useContext(UserContext)
   /* 查询参数 */
   const reducer = (state, action) => {
     switch (action.type) {
@@ -20,120 +20,120 @@ function Report() {
           ...state,
           keyword: action.payload,
           page: 0,
-        };
+        }
       case 'type':
         return {
           ...state,
           mediaType: action.payload,
           page: 0,
-        };
+        }
       case 'time':
         return {
           ...state,
           date: action.payload,
           page: 0,
-        };
+        }
       case 'page':
         return {
           ...state,
           page: action.payload,
-        };
+        }
       case 'size':
         return {
           ...state,
           size: action.payload,
           page: 0,
-        };
+        }
       default:
-        return state;
+        return state
     }
-  };
+  }
   const initialParams = {
     keyword: '',
     mediaType: '',
     date: '',
     page: 0,
     size: 10,
-  };
-  const [params, dispatchParams] = useReducer(reducer, initialParams);
-  const [totalNum, setTotalNum] = useState('');
-  const totalNumRef = useRef(null); // 左上角总数
-  const [forceUpdate, setForceUpdate] = useState(0); // 操作后更新表格
+  }
+  const [params, dispatchParams] = useReducer(reducer, initialParams)
+  const [totalNum, setTotalNum] = useState('')
+  const totalNumRef = useRef(null) // 左上角总数
+  const [forceUpdate, setForceUpdate] = useState(0) // 操作后更新表格
   useEffect(() => {
     // 获取表格数据
-    console.log(params);
+    console.log(params)
 
-    setIsShowLoading(true);
+    setIsShowLoading(true)
     async function getTabledata() {
-      let res = await newsQuery(params);
-      console.log(res);
+      let res = await newsQuery(params)
+      console.log(res)
       if (totalNumRef.current === null) {
-        totalNumRef.current = res.totalElements;
+        totalNumRef.current = res.totalElements
       }
-      setTotalNum(res.totalElements);
-      setDataSource(res.content);
-      setIsShowLoading(false);
+      setTotalNum(res.totalElements)
+      setDataSource(res.content)
+      setIsShowLoading(false)
     }
-    getTabledata();
-  }, [params, forceUpdate]);
+    getTabledata()
+  }, [params, forceUpdate])
 
   // 获取所有媒体
-  const [allMedia, setAllMedia] = useState([]);
+  const [allMedia, setAllMedia] = useState([])
   useEffect(() => {
     async function getMedia() {
-      let res = await mediaQuery({ all: 'all' });
-      console.log('所有媒体', res.content);
-      setAllMedia(res.content);
+      let res = await mediaQuery({ all: 'all' })
+      console.log('所有媒体', res.content)
+      setAllMedia(res.content)
     }
-    getMedia();
-  }, []);
+    getMedia()
+  }, [])
 
   /* 搜索 */
   const handleChangeSearch = (e) => {
-    console.log('搜索框变动', e.target.value);
+    console.log('搜索框变动', e.target.value)
     dispatchParams({
       type: 'text',
       payload: e.target.value.replace("'", '').trim(),
-    });
-  };
+    })
+  }
 
   /* 增改报道对话框 */
-  const [isShowAdd, setIsShowAdd] = useState(false);
-  const addMediaRef = useRef(null);
+  const [isShowAdd, setIsShowAdd] = useState(false)
+  const addMediaRef = useRef(null)
   const getMediaRef = useCallback((node) => {
     // 动态获取每次重载的 增改 对话框 DOM
-    if (node) addMediaRef.current = node;
-  }, []);
+    if (node) addMediaRef.current = node
+  }, [])
   const handleCancelAddModal = () => {
-    setIsShowAdd(false);
-    setCurrentMediaType('');
-  };
+    setIsShowAdd(false)
+    setCurrentMediaType('')
+  }
 
   // 媒体选择
-  const [currentMediaType, setCurrentMediaType] = useState(''); // 保存选中、回显媒体信息
+  const [currentMediaType, setCurrentMediaType] = useState('') // 保存选中、回显媒体信息
   const handleSelectMedia = (id, option) => {
     if (id) {
-      let mediaName = option.children;
-      let key = allMedia.filter((item) => item.id === id)[0]['mediaType'];
-      let label = mediaType.filter((item) => item.key === key)[0]['label'];
-      setCurrentMediaType((state) => ({ ...state, mediaName, key, label }));
+      let mediaName = option.children
+      let key = allMedia.filter((item) => item.id === id)[0]['mediaType']
+      let label = mediaType.filter((item) => item.key === key)[0]['label']
+      setCurrentMediaType((state) => ({ ...state, mediaName, key, label }))
     } else {
-      setCurrentMediaType('');
+      setCurrentMediaType('')
     }
-  };
+  }
 
   // 日期选择
   const handleTimeChange = (date, dateString) => {
-    console.log(date, dateString);
-  };
+    console.log(date, dateString)
+  }
   // 确定
-  const [modalType, setModalType] = useState('publishStatus'); // 启用禁用首次不执行状态、弹窗，指定默认状态标识为发布状态
+  const [modalType, setModalType] = useState('publishStatus') // 启用禁用首次不执行状态、弹窗，指定默认状态标识为发布状态
   const HandleAddOrChangeMedia = () => {
     addMediaRef.current
       .validateFields()
       .then((res) => {
-        console.log(res);
-        let params;
+        console.log(res)
+        let params
         switch (modalType) {
           case 'add':
             params = [
@@ -148,9 +148,9 @@ function Report() {
                 publishFlag: 1,
                 creator: user.userId,
               },
-            ];
-            addNews(params);
-            break;
+            ]
+            addNews(params)
+            break
           case 'change':
             // console.log('不是change么');
             params = [
@@ -166,55 +166,55 @@ function Report() {
                 publishFlag: 1,
                 updator: user.userId,
               },
-            ];
-            console.log(params);
-            updateNews(params);
-            break;
+            ]
+            console.log(params)
+            updateNews(params)
+            break
           default:
         }
       })
       .catch((err) => {
-        console.log(err);
-      });
-  };
+        console.log(err)
+      })
+  }
 
   // 新增
   async function addNews(params) {
-    let res = await newsAdd(params);
-    console.log(res);
-    message.success('添加成功', 1);
-    setIsShowAdd(false);
-    setForceUpdate((count) => count + 1);
+    let res = await newsAdd(params)
+    console.log(res)
+    message.success('添加成功', 1)
+    setIsShowAdd(false)
+    setForceUpdate((count) => count + 1)
   }
   // 删改
-  let currentPublishStatus; // 判断启用禁用
+  let currentPublishStatus // 判断启用禁用
   async function updateNews(params) {
-    let res = await newsUpdate(params);
-    console.log(res);
+    let res = await newsUpdate(params)
+    console.log(res)
     if (modalType === 'change') {
-      message.success('修改报道成功', 1);
-      setIsShowAdd(false);
-      setCurrentMediaType('');
+      message.success('修改报道成功', 1)
+      setIsShowAdd(false)
+      setCurrentMediaType('')
     } else if (modalType === 'delete') {
-      message.success('删除报道成功', 1);
-      setIsShowDelete(false);
+      message.success('删除报道成功', 1)
+      setIsShowDelete(false)
     } else if (modalType === 'publishStatus') {
-      console.log('首次无弹窗，是否执行');
+      console.log('首次无弹窗，是否执行')
       if (currentPublishStatus === 1) {
-        message.destroy();
-        message.success('禁用报道成功', 2);
+        message.destroy()
+        message.success('禁用报道成功', 2)
       }
       if (currentPublishStatus === 0) {
-        message.destroy();
-        message.success('启用报道成功', 2);
+        message.destroy()
+        message.success('启用报道成功', 2)
       }
     }
-    setForceUpdate((count) => count + 1);
+    setForceUpdate((count) => count + 1)
   }
 
   // 表格
-  const [dataSource, setDataSource] = useState([]);
-  const [isShowLoading, setIsShowLoading] = useState(false);
+  const [dataSource, setDataSource] = useState([])
+  const [isShowLoading, setIsShowLoading] = useState(false)
 
   // for (let index = 0; index < 100; index++) {
   // 	dataSource.push({
@@ -250,8 +250,8 @@ function Report() {
       label: '其它',
       key: 5,
     },
-  ];
-  const year = new Date().getFullYear();
+  ]
+  const year = new Date().getFullYear()
   const postTime = [
     {
       label: '全部',
@@ -277,15 +277,15 @@ function Report() {
       label: '其它',
       key: 'other',
     },
-  ];
+  ]
   const handleClickType = (item) => {
-    console.log('点击筛选媒体类型：', item);
-    dispatchParams({ type: 'type', payload: item.key });
-  };
+    console.log('点击筛选媒体类型：', item)
+    dispatchParams({ type: 'type', payload: item.key })
+  }
   const handleClickTime = (item) => {
-    console.log('点击筛选发布时间：', item);
-    dispatchParams({ type: 'time', payload: item.key });
-  };
+    console.log('点击筛选发布时间：', item)
+    dispatchParams({ type: 'time', payload: item.key })
+  }
 
   const columns = [
     {
@@ -320,7 +320,7 @@ function Report() {
                 {mediaType.map((item) => (
                   <Menu.Item
                     onClick={(item) => {
-                      handleClickType(item);
+                      handleClickType(item)
                     }}
                     key={item.key}
                   >
@@ -363,7 +363,7 @@ function Report() {
                 {postTime.map((item) => (
                   <Menu.Item
                     onClick={(item) => {
-                      handleClickTime(item);
+                      handleClickTime(item)
                     }}
                     key={item.key}
                   >
@@ -404,28 +404,28 @@ function Report() {
               className="delete iconfont icon-lajitong"
             ></span>
           </div>
-        );
+        )
       },
     },
-  ];
+  ]
 
   // 编辑
   const handleClickEdit = (data) => {
-    console.log('编辑当前行', data);
-    setIsShowAdd(true);
-    setModalType('change');
+    console.log('编辑当前行', data)
+    setIsShowAdd(true)
+    setModalType('change')
 
-    let mediaName = data.media;
-    let key = data.mediaType;
-    let label = mediaType.filter((item) => item.key === key)[0]['label'];
+    let mediaName = data.media
+    let key = data.mediaType
+    let label = mediaType.filter((item) => item.key === key)[0]['label']
 
-    console.log(data.id);
+    console.log(data.id)
     setCurrentMediaType({
       mediaName,
       key,
       label,
       id: data.id, // 保存当前行新闻 id ，作为删改参数
-    });
+    })
 
     // console.log('点击时是否获取到Form', addMediaRef);
     setTimeout(() => {
@@ -436,23 +436,23 @@ function Report() {
           mediaType: data.address,
           link: data.url,
           time: dayjs(data.newsDate),
-        });
-    });
-  };
+        })
+    })
+  }
 
   // 删除对话框
-  const [isShowDelete, setIsShowDelete] = useState(false); // 是否展示
-  const [currentMediaData, setCurrentMediaData] = useState({}); // 当前媒体数据
+  const [isShowDelete, setIsShowDelete] = useState(false) // 是否展示
+  const [currentMediaData, setCurrentMediaData] = useState({}) // 当前媒体数据
   const handleClickDelete = (data) => {
-    console.log('删除当前行', data);
-    setCurrentMediaData(data);
-    setIsShowDelete(true);
-    setModalType('delete');
-  };
+    console.log('删除当前行', data)
+    setCurrentMediaData(data)
+    setIsShowDelete(true)
+    setModalType('delete')
+  }
   const handleCancelDeleteModal = () => {
-    setIsShowDelete(false);
-    setCurrentMediaData({});
-  };
+    setIsShowDelete(false)
+    setCurrentMediaData({})
+  }
   const HandleDeleteMedia = () => {
     // 确认删除当前新闻
     let params = [
@@ -468,29 +468,29 @@ function Report() {
         publishFlag: 0,
         updator: user.userId,
       },
-    ];
-    updateNews(params);
-  };
+    ]
+    updateNews(params)
+  }
 
   // 切换报道禁用使用状态
   const handleClickChangeStatus = (item) => {
-    setModalType('publishStatus');
-    currentPublishStatus = item.publishFlag;
-    let params = { ...item };
-    console.log('启用禁用报道按钮：', item);
-    console.log(params);
-    delete params.createTime;
-    delete params.creator;
-    delete params.updateTime;
-    params.updator = user.userId;
+    setModalType('publishStatus')
+    currentPublishStatus = item.publishFlag
+    let params = { ...item }
+    console.log('启用禁用报道按钮：', item)
+    console.log(params)
+    delete params.createTime
+    delete params.creator
+    delete params.updateTime
+    params.updator = user.userId
     if (params.publishFlag) {
-      params.publishFlag = 0;
+      params.publishFlag = 0
     } else {
-      params.publishFlag = 1;
+      params.publishFlag = 1
     }
-    console.log(params);
-    updateNews([params]);
-  };
+    console.log(params)
+    updateNews([params])
+  }
 
   return (
     <div className="report">
@@ -512,8 +512,8 @@ function Report() {
           />
           <span
             onClick={() => {
-              setIsShowAdd(!isShowAdd);
-              setModalType('add');
+              setIsShowAdd(!isShowAdd)
+              setModalType('add')
             }}
             className="iconfont icon-add-sy"
           />
@@ -540,12 +540,12 @@ function Report() {
             showTotal: (total) => `共 ${total} 条`,
             showSizeChanger: true,
             onShowSizeChange: (current, size) => {
-              console.log('改变每页条数', current, size);
-              dispatchParams({ type: 'size', payload: size });
+              console.log('改变每页条数', current, size)
+              dispatchParams({ type: 'size', payload: size })
             },
             onChange: (page, pageSize) => {
-              console.log('点击分页:', page, pageSize);
-              dispatchParams({ type: 'page', payload: page - 1 });
+              console.log('点击分页:', page, pageSize)
+              dispatchParams({ type: 'page', payload: page - 1 })
             },
           }}
         />
@@ -626,7 +626,7 @@ function Report() {
               inputReadOnly={true}
               onChange={handleTimeChange}
               disabledDate={(current) => {
-                return current && current >= dayjs().endOf('day');
+                return current && current >= dayjs().endOf('day')
               }}
             />
           </Form.Item>
@@ -669,7 +669,7 @@ function Report() {
         </div>
       </Modal>
     </div>
-  );
+  )
 }
 
-export default Report;
+export default Report
